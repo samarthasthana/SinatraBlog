13990c49e6a0672506afb6faba860c223cfaad39
samarthasthana <samarth.asthana@gmail.com>
2014-01-29 22:58:11 -0500

    All post and Add and Edit

--- myapp.rb
+++ myapp.rb
@@ -17,10 +17,18 @@ key :content, String
 end
 
 get '/' do
-params[:posts]=Post.find_by_name("Sagar")
 @test=Post.all()
 #Post.find_by_name("Sam")
 erb :index
+end
+
+post '/' do
+pos=Post.new
+pos.topic=params[:topic]
+pos.name=params[:name]
+pos.content=params[:txt]
+pos.save
+erb:index 
 end
 
 get '/add/?' do
